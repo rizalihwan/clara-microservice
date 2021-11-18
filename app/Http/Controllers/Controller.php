bcb21bc9e4017aa86e7e31b3e3b49d106df6d31b
rizalihwan <rizalihwan94@gmail.com>
2021-11-18 15:23:32 +0700

    add public respond the error message

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function errorMessageRespond($message, int $statusCode)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => "Error: {$message}"
+        ], $statusCode);
+    }
 }
